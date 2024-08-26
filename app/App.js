1d6b9ecf0295e2ce04d0e34fb7c4bc2706df8236
Jerome Otto <jeromeo1969@hotmail.com>
2024-08-26 15:40:57 -0600

    Initial Framework

--- app/App.js
+++ app/App.js
@@ -1,10 +1,12 @@
 import { router } from './router-config.js';
 import { HomeController } from './controllers/HomeController.js';
+import { PlayersController } from './controllers/PlayersController.js';
 const USE_ROUTER = false
 
 class App {
 
   HomeController = new HomeController()
+  PlayersController = new PlayersController
 
   constructor() {
     if (USE_ROUTER) {
